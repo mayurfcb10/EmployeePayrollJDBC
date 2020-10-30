927729f652efb69ea16b21e1a3e463b05d912610
Mayur <mayurzope555@gmail.com>
2020-10-31 00:44:40 +0530

    [Mayur]Added: Updated basic pay for the Employee in the database

--- src/main/java/com/bridgelabz/employeeservice/EmployeePayroll/PayrollServiceException.java
+++ src/main/java/com/bridgelabz/employeeservice/EmployeePayroll/PayrollServiceException.java
@@ -2,7 +2,7 @@ package com.bridgelabz.employeeservice.EmployeePayroll;
 
 public class PayrollServiceException extends Exception{
 	enum ExceptionType{
-		CONNECTION_PROBLEM, RETRIEVAL_PROBLEM; 
+		CONNECTION_PROBLEM, RETRIEVAL_PROBLEM, UPDATE_PROBLEM; 
 	}
 	
 	ExceptionType type;

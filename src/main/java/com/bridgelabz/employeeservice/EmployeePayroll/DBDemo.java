619be1acb6b9687131597112fb9f6b700df8ab16
Mayur <mayurzope555@gmail.com>
2020-10-30 20:00:42 +0530

    [Mayur]Added: Welcome to the Employee Payroll JDBC Program

---
+++ src/main/java/com/bridgelabz/employeeservice/EmployeePayroll/DBDemo.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeeservice.EmployeePayroll;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo 
+{
+	public static void printWelcomeMessage() {
+		System.out.println("Welcome to Employee Payroll JDBC ");
+		
+	}
+}
---
+++ src/test/java/com/bridgelabz/employeeservice/EmployeePayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeeservice.EmployeePayroll;
+
+import org.junit.Test;
+
+public class EmployeePayrollServiceTest {
+		@Test
+		public void WelcomeMessage() {
+			DBDemo.printWelcomeMessage();
+		}
+	}

6073452dc3023408b07d4d5adb1a3f7c5a5b8645
Mayur <mayurzope555@gmail.com>
2020-10-31 10:27:38 +0530

    [Mayur]Added: Ability to retireve employee joined in a patricular date range from service_payroll database

--- src/main/java/com/bridgelabz/employeeservice/EmployeePayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeeservice/EmployeePayroll/EmployeePayrollService.java
@@ -94,9 +94,7 @@ public class EmployeePayrollService {
 	private EmployeePayrollData getEmployeePayrollData(String name) {
 		EmployeePayrollData employeePayrollData;
 		employeePayrollData = this.employeePayrollList.stream()
-				.filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name))
-				.findFirst()
-				.orElse(null);
+				.filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name)).findFirst().orElse(null);
 		return employeePayrollData;
 	}
 
